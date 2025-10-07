5f994967ee2f4207c5e50c0f20953ad8c5528b7b
Lucas Alves <lucascalliari172@gmail.com>
2025-10-07 12:45:16 -0300

    Adicionando primeiros itens de teste.

--- src/main/java/com/luvas/tutorialmod/TutorialMod.java
+++ src/main/java/com/luvas/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package com.luvas.tutorialmod;
 
+import com.luvas.tutorialmod.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -36,6 +38,8 @@ public class TutorialMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -51,7 +55,11 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+            event.accept(ModItems.GOOFY_AHH_GEM);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
